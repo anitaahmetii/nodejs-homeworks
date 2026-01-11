ac9ad4cbf4061753e4e4f5d7619aaddabec95e1d
Anita Ahmeti <aniita.ahmeti@gmail.com>
2026-01-12 00:56:05 +0100

    Add login authorization with POST parsing using querystring.

--- HW-05/server.js
+++ HW-05/server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const fs = require('node:fs');
 const path = require('node:path');
+const querystring = require('node:querystring');
 
-const server = http.createServer((request, response) => {
+const server = http.Server((request, response) => {
     const htmlPath = path.join(__dirname, 'login.html');
-    if (request.url === '/login.html' && request.method === 'GET')
+    if (request.url === '/' && request.method === 'GET')
     {
         response.setHeader('Content-Type', 'text/html');
         fs.readFile(htmlPath, 'utf-8', (error, data) => {
@@ -12,18 +13,36 @@ const server = http.createServer((request, response) => {
                 response.end('<h1>Internal Server Error</h1>');
                 return;
             }
-            else return response.end(`${data} <h3>You are using ${request.method} method!</h3>`);
+            else return response.end(data);
         });
         return;
     }
     else if (request.url === '/login' && request.method === 'POST')
     {
-        response.end(`<h1>Success</h1> <h3>You were using ${request.method} method!</h3>`);
+        let body = "";
+        request.on('data', chunk => {
+            body += chunk;
+        });
+        request.on('end', () => {
+            const parsedData = querystring.parse(body);
+            const email = parsedData.email;
+            const password = parsedData.password;
+
+            if (email === 'user@user.com' && password === 'secret')
+            {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.end(`<h1>Logged in successfully<h1> <h3><br>Email: <i>${email}</i> <br> Password: <i>${password}</i></h3>`);
+            }
+            else 
+            { 
+                response.end("Not authorized");
+            }
+
+        });
         return;
     }
- 
     response.statusCode = 404;
-    response.end("<h1>This page does not exist!</h1>");
+    response.end("404 Not Found");
 });
 
 server.listen(3000, () => {
